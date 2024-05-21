69a2bc6cc3397606fd3020d99b61296aadd72638
Jair0818 <naruhina0818@gamil.com>
2024-05-21 09:45:00 -0600

    primer avanse

--- ProyectoFinal.15/Program.cs
+++ ProyectoFinal.15/Program.cs
@@ -1,1 +1,36 @@
 ﻿using System;
+public class ProyectoFinal
+{
+    public static void Main(string[] args)
+    {
+        string pathUusarios = "Uusarios.txt";
+        string pathLibros = "Libros.txt";
+        VerificaDoc(pathUusarios);
+        VerificaDoc(pathLibros);
+        LogIn(pathUusarios);
+        LogIn(pathLibros);
+    }
+    public static void VerificaDoc(string path)
+    {
+        if (File.Exists(path))
+        {
+            Console.WriteLine("El archivo no existe, creando documentacion necesaria...");
+            CrearDoc(path);
+            Console.WriteLine("Documentacion creada con éxito");
+        }
+    }
+    public static void CrearDoc(string path)
+    {
+        using (StreamWriter crear = File.CreateText(path))
+        {
+            crear.WriteLine("Bienvenido a la Biblioteca LoveBook");
+        }
+    }
+    public static string LecturaDoc(string path)
+    {
+        using(StreamReader lectura = File.OpenText(path))
+        {
+            return lectura.ReadToEnd();
+        }
+    }
+}
