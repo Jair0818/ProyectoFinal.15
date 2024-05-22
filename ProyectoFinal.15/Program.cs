ef655bf98aa49fe0b0cc83365d2f4775574e8857
Jair0818 <naruhina0818@gamil.com>
2024-05-22 12:29:36 -0600

    yei1
    Co-authored-by: KarinaJA13 <KarinaJA13@users.noreply.github.com>

--- ProyectoFinal.15/Program.cs
+++ ProyectoFinal.15/Program.cs
@@ -82,7 +82,7 @@ public class Programa
         Console.Clear();
         string psw = EntradaDatos("Ingresa Contraseña");
         Console.Clear();
-        int userFlow = EncontrarCasilla(datos, id);
+        int userFlow = EncontrarCasillaMatriz(datos, id);
         if (userFlow >= 0)
         {
             if (datos[userFlow, 3].Trim().Equals(psw))
@@ -133,26 +133,35 @@ public class Programa
         Console.WriteLine("Ingresa la opción deseada: ");
         Console.WriteLine("1-ALta de Usuarios");
         Console.WriteLine("2-Baja de Usuarios");
-        Console.WriteLine("4-Cambio de Contraseña");
-        Console.WriteLine("5-Solicitar Prestamos");
-        Console.WriteLine("6-Buscar Libro");
-        Console.WriteLine("-Salir");
+        Console.WriteLine("3-Cambio de Contraseña");
+        Console.WriteLine("4-Solicitar Prestamos");
+        Console.WriteLine("5-Buscar Libro");
+        Console.WriteLine("6-Salir");
     }
     public static void MenuLibro()
     {
-        string Entrada = EntradaMenu("¿Cómo quieres buscar tu libro?");
-        switch (Entrada)
-        {
-            case "ID":
-            
+        Console.WriteLine("1-Buscar libro por ID");
+        Console.WriteLine("2-Buscar libro por Autor");
+        Console.WriteLine("3-Buscar libro por el Nombre del Libro");
+        Console.WriteLine("4-Buscar libro por la Categoria");
+        Console.WriteLine("5-Buscar libro por la Editorial");
+        Console.WriteLine("6-Buscar libro por el Idioma");
+        Console.Clear();
+        string lib= EntradaDatos("Ingresa una opcion:");
+        switch (lib){
+            case"1"://id
             break;
-            case "Autor":
-            
+            case"2"://autor
             break;
-            case "":
-            
+            case"3"://libro
             break;
-        } 
+            case"4"://categoria
+            break;
+            case"5"://editorial
+            break;
+            case"6"://idioma
+            break;
+        }
     }
     public static void Ejecucion(string path, string[,] datos)
     {
@@ -170,34 +179,19 @@ public class Programa
                 DarBaja(datos);
                 EscribirCambios(path, datos);
                 break;
-            case "3"://nombre
-                Console.Clear();
-                string exp = EntradaDatos("Ingresa tu expediente: ");
-                break;
             case "4"://contraseña
                 Console.Clear();
-                exp = EntradaDatos("Ingresa tu expediente: ");
+                string exp = EntradaDatos("Ingresa tu expediente: ");
                 break;
             case "5"://prestamos
                 Console.Clear();
                 exp = EntradaDatos("Ingresa tu expediente: ");
                 //CambiarDato(exp, arregloPalabras, path, 4);
                 break;
-            case "6": //id libro
-                exp = EntradaDatos("Ingresa tu expediente: ");
-                //CambiarDato(exp, arregloPalabras, path, 2);
+            case "6": //buscar libro
+                MenuLibro();
                 break;
-            case "7"://autor libro
-                string filter = EntradaDatos("Ingresa el filtro: ");
-                Filtrado(datos, filter);
-                break;
-            case "8"://nombre libro:
-                break;
-            case "9"://categoria
-                break;
-            case "10":
-                break;
-            default:
+            case "7"://salir
                 break;
         }
     }
@@ -217,9 +211,50 @@ public class Programa
         }
         return matriz;
     }
+    public static string[,] CrearMatrizLibro(string path2)
+    {
+        string texto = LecturaDocs(path2); //Obteniendo el texto del txt
+        string[] filas = texto.Split('\n');
+        string[,] matriz = new string[filas.Length, 3];
+        for (int i = 0; i < filas.Length; i++)
+        {
+            string[] datosDeFila = filas[i].Split(',');
+            for (int j = 0; j < datosDeFila.Length; j++)
+            {
+                matriz[i, j] = datosDeFila[j].Trim();
+            }
+        }
+        return matriz;
+    }
     public static void EscribirCambiosMatriz(string path, string[,] arreglo)
     {
         StreamWriter escritura = File.CreateText(path);
+        for (int j = 0; j < arreglo.GetLength(0); j++)
+        {
+            for (int i = 0; i < arreglo.GetLength(1); i++)
+            {
+                if (i == arreglo.GetLength(1) - 1)
+                {
+                    escritura.Write(arreglo[j, i]);
+                }
+                else
+                {
+                    escritura.Write(arreglo[j, i]);
+                    escritura.Write(','); //va a existir una coma al final
+                }
+
+            }
+            if (!(j == arreglo.GetLength(0) - 1))
+            {
+                escritura.WriteLine();
+            }
+        }
+
+        escritura.Close();
+    }
+    public static void EscribirCambiosLibros(string path2, string[,] arreglo)
+    {
+        StreamWriter escritura = File.CreateText(path2);
         for (int j = 0; j < arreglo.GetLength(0); j++)
         {
             for (int i = 0; i < arreglo.GetLength(1); i++)
@@ -276,6 +311,25 @@ public class Programa
             Console.WriteLine("Expediente repetido, imposible dar de alta");
         }
     }
+    public static void DarAltaLibros(string[,] datos)
+    {
+        //Verificar que el expediente no exista en la BD
+        String altaExp = EntradaDatos("Ingresa el expediente a agregar: ");
+        if (EncontrarCasillaMatriz(datos, altaExp) < 0) //
+        {
+            string datosUsuario = EntradaDatos("Ingresa tu nombre, carrera y contraseña separado por comas (,): ");
+            string[] filaAgregar = datosUsuario.Split(',');
+            datos[datos.GetLength(0) - 1, 0] = altaExp;
+            for (int i = 1; i < datos.GetLength(1); i++)
+            {
+                datos[datos.GetLength(0) - 1, i] = filaAgregar[i - 1];
+            }
+        }
+        else
+        {
+            Console.WriteLine("Expediente repetido, imposible dar de alta");
+        }
+    }
     public static void CambiarDatoMatriz(string exp, string[,] arregloPalabras, string path, int x)
     {
         //Buscar al expediente que vamos a modificar
@@ -292,13 +346,33 @@ public class Programa
             Console.WriteLine("Usuario no encontrado");
         }
     }
-    
+     public static void CambiarDatoLibros(string exp, string[,] arregloPalabras, string path2, int x)
+    {
+        //Buscar al expediente que vamos a modificar
+        int filaUsuario = -1;
+        filaUsuario = EncontrarCasillaMatriz(arregloPalabras, exp); //va dar un número > 0 si existe le user
+        if (filaUsuario >= 0)
+        {
+            string entrada = EntradaDatos("Ingresa el nuevo valor: ");
+            arregloPalabras[filaUsuario, x] = entrada;
+            EscribirCambios(path2, arregloPalabras);
+        }
+        else
+        {
+            Console.WriteLine("Usuario no encontrado");
+        }
+    }
     public static string EntradaDatos(string texto)
     {
         Console.WriteLine(texto);
         return Console.ReadLine();
     }
-    public static int EncontrarCasilla(string[,] arreglo, string expediente)
+    public static string EntradaDatosLibros(string texto)
+    {
+        Console.WriteLine(texto);
+        return Console.ReadLine();
+    }
+    public static int EncontrarCasillaLibros(string[,] arreglo, string expediente)
     {
         for (int i = 0; i < arreglo.GetLength(0); i++)
         {
@@ -309,11 +383,11 @@ public class Programa
         }
         return -1;
     }
-    public static void Filtrado(string[,] datos, string filtro)
+    public static void Filtrado(string[,] datos, string filtro, int x)
     {
         for (int i = 0; i < datos.GetLength(0) - 1; i++)
         {
-            if (datos[i, 2].ToLower().Contains(filtro.ToLower()))
+            if (datos[i, ].ToLower().Contains(filtro.ToLower()))
             {
                 for (int j = 0; j < datos.GetLength(1); j++)
                 {
@@ -376,6 +450,27 @@ public class Programa
     public static void EscribirCambiosAlta(string path, string[,] arreglo)
     {
         StreamWriter escritura = File.CreateText(path);
+        for (int j = 0; j < arreglo.GetLength(0); j++)
+        {
+            for (int i = 0; i < arreglo.GetLength(1); i++)
+            {
+                if (i == arreglo.GetLength(1) - 1)
+                {
+                    escritura.Write(arreglo[j, i]);
+                }
+                else
+                {
+                    escritura.Write(arreglo[j, i]);
+                    escritura.Write(',');  //Va a existir una coma al final
+                }
+            }
+            escritura.WriteLine();
+        }
+        escritura.Close();
+    }
+     public static void EscribirCambiosAltaLibros(string path2, string[,] arreglo)
+    {
+        StreamWriter escritura = File.CreateText(path2);
         for (int j = 0; j < arreglo.GetLength(0); j++)
         {
             for (int i = 0; i < arreglo.GetLength(1); i++)
